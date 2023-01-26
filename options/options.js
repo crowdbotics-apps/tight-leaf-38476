a2f78f8867a3052ec2564d3588fa45b02de6a30d
Crowdbotics <team@crowdbotics.com>
2023-01-26 06:33:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-leaf-38476",
+  url: "https://tight-leaf-38476.botics.co",
+  api: "https://tight-leaf-38476.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
